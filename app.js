fc78f23c092c42868b2de927c3f2ee2f47f66f09
Matias2022 <mattias.benitez@gmail.com>
2022-07-19 09:45:32 -0300

    weather api included

--- app.js
+++ app.js
@@ -10,7 +10,65 @@ let LIST
 
 //creacion de fecha
 const FECHA = new Date()
-fecha.innerHTML = FECHA.toLocaleDateString('es-AR',{weekday:'long',month:'short',day:'numeric'})
+fecha.innerHTML = FECHA.toLocaleDateString('es-AR',{weekday:'long',month:'long',day:'numeric'})
+
+//Clima
+window.addEventListener('load', ()=> {
+    let lon 
+    let lat 
+    let temperaturaValor = document.getElementById('temperatura-valor')
+    let temperaturaDescripcion = document.getElementById('temperatura-descripcion')
+    let ubicacion = document.getElementById('ubicacion')
+    let iconoAnimado = document.getElementById('icono-animado')
+    let vientoVelocidad = document.getElementById('viento-velocidad')
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( posicion => {
+            lon = posicion.coords.longitude
+            lat = posicion.coords.latitude
+
+            //ubicacion actual
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=839a3bf3798c48a3df5195e3151f4b73`
+
+            fetch(url)
+                .then( response => {return response.json();})
+                .then( data => {
+                    let temp = Math.round(data.main.temp)
+                    temperaturaValor.textContent = `${temp}°C`
+                    let desc = data.weather[0].description;
+                    temperaturaDescripcion.textContent = desc.toUpperCase()
+                    ubicacion.textContent = data.name
+                    vientoVelocidad.textContent = `${data.wind.speed} m/s`
+
+                    switch (data.weather[0].main) {
+                        case 'Thunderstorm':
+                          iconoAnimado.src='animated/thunder.svg'
+                          break;
+                        case 'Drizzle':
+                          iconoAnimado.src='animated/rainy-2.svg'
+                          break;
+                        case 'Rain':
+                          iconoAnimado.src='animated/rainy-7.svg'
+                          break;
+                        case 'Snow':
+                          iconoAnimado.src='animated/snowy-6.svg'
+                          break;                        
+                        case 'Clear':
+                            iconoAnimado.src='animated/day.svg'
+                          break;
+                        case 'Atmosphere':
+                          iconoAnimado.src='animated/weather.svg'
+                            break;  
+                        case 'Clouds':
+                            iconoAnimado.src='animated/cloudy-day-1.svg'
+                            break;  
+                        default:
+                          iconoAnimado.src='animated/cloudy-day-1.svg'
+                      }
+                })
+        })
+    }
+})
 
 
 //funcion agregar tarea
